ccc7a68b60a152f698711e5be11d1ecbc7c41ae1
Edo <eduard.kubinec@gmail.com>
2022-01-02 11:29:08 +0100

    delete unnecessary sql file

--- ToDoApp/db.sql
+++
@@ -1,6 +0,0 @@
-﻿-- this is how DB looks like - nHibernate can also create DB from models automagically
-CREATE TABLE TODO (
-    ID int IDENTITY(1,1) PRIMARY KEY,
-    NAME varchar(255) NOT NULL,
-    TEXT varchar(255)
-);
